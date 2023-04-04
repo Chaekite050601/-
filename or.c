366dc7903d8a6f444835da569270cd582342971c
31019 <>
2023-04-04 05:04:54 +0000

    condition_if.c

--- or.c
+++ or.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
-main()
-{
+// #include <stdio.h>
+// main()
+// {
 
-  int a=15;
-  int b=20;
-  int c = a|b;
+//   int a=15;
+//   int b=20;
+//   int c = a|b;
 
 
-  printf("OR 연산 결과 : %d", c );
-}
+//   printf("OR 연산 결과 : %d", c );
+// }
