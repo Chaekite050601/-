8df095722aec72a0f0a99f25d88798f2012d1665
Chaekite050601 <128569866+Chaekite050601@users.noreply.github.com>
2023-05-16 06:25:06 +0000

    조건문 끝!

---
+++ mission_myscore.c
@@ -0,0 +1,18 @@
+// #include <stdio.h>
+// main(){
+//  int k;
+//   printf("A=k>89 | B=k>79 | C=k>69 | D=k>59\n");
+//   scanf("%d", &k);
+  
+//   if(k>89)
+//     printf("A");
+//   else if(k>79)
+//     printf("B");
+//   else if(k>69)
+//     printf("C");
+//       else if(k>59)
+//       printf("D");
+//         else
+//       printf("F");
+//   printf("채점 끝");
+// } 
